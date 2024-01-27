7991621cf0ea0d7ce1fb5d4cf67254e0e23ed4fd
ibtissammassa <bettysama16@gmail.com>
2024-01-27 21:53:39 +0100

    NavBar

--- app/Components/NavBar.jsx
+++ app/Components/NavBar.jsx
@@ -8,11 +8,19 @@ function NavBar() {
     return ( 
         <div className="flex justify-between sticky px-20 py-10 items-center">
             <h3 className="font-bold text-lg">Ibtissam M.</h3>
-            <ul className="flex gap-6 font-light text-sm">
-                <li>About</li>
-                <li>Tech Stack</li>
-                <li>Projects</li>
-                <li>Contact</li>
+            <ul className="flex gap-7 font-light text-sm">
+                <li className="hover:text-green-400 cursor-pointer">
+                    <Link href="#">About</Link>
+                </li>
+                <li className="hover:text-green-400 cursor-pointer">
+                    <Link href="#">Tech Stack</Link>
+                </li>
+                <li className="hover:text-green-400 cursor-pointer">
+                    <Link href="#">Projects</Link>
+                </li>
+                <li className="hover:text-green-400 cursor-pointer">
+                    <Link href="#">Contact</Link>
+                </li>
             </ul>
             <Link className="bg-white inline-flex rounded-lg px-2.5 py-1.5 gap-2" href="/">
                 <p className="text-black text-sm">Resume</p>
